a4dd80f28d7b39be9c8a52bf97e400dc40adddaf
mengi <osnovanskii@gmail.com>
2024-12-02 13:47:27 +0700

    Реакции на кнопки

--- main.py
+++ main.py
@@ -13,7 +13,21 @@ menu.add(write,events,delete,edit)
 back = types.ReplyKeyboardMarkup(resize_keyboard=True)
 back_button = types.KeyboardButton("Назад")
 back.add(back_button)
+#реакции на кнопки
+@bot.message_handler(context_types=['text'])
+def text_messages(message):
+    if message.text == "Назад":
+        bot.send_message(message.chat.id, "Что вас интересует?", reply_markup = menu)
+    elif message.text == "События":
+        bot.send_message(message.chat.id,"Записанные события", reply_markup = back)
+    elif message.text == "Записать событие":
+        bot.send_message(message.chat,id, "Введите данные", reply_markup = types.ReplyKeyboardRemove() )
+    elif message.text == "Удалить событие":
+        bot.send_message(message.chat.id, "Выберите событие которое нужно удалить", reply_markup = back)
+    elif message.text == "Редактировать событие":
+        bot.send_message(message.chat.id, "Выберите событие которое нужно редактировать", reply_markup = back)
 
+    
 # реакция на команду start
 @bot.message_handler(commands=['start'])
 def start_message(message):
